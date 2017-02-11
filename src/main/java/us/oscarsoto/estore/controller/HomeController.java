804b168b481eec85dd14594b1729ae36d70a7859
Oscar Soto <oscarasotoa@gmail.com>
2017-02-11 16:33:36 -0600

    Initial commit

---
+++ src/main/java/us/oscarsoto/estore/controller/HomeController.java
@@ -0,0 +1,35 @@
+package us.oscarsoto.estore.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import us.oscarsoto.estore.dao.ProductDao;
+import us.oscarsoto.estore.model.Product;
+
+import java.util.List;
+
+/**
+ * @author oscarsoto on 2/11/17.
+ *         There is no reasonable excuse for doing anything less than your best.
+ *         - Martin, Robert C.
+ */
+
+@Controller
+public class HomeController {
+
+    private ProductDao productDao = new ProductDao();
+
+    @RequestMapping("/")
+    public String home() {
+        return "home";
+    }
+
+    @RequestMapping("/productList")
+    public String getProducts(Model model){
+        List<Product> products = productDao.getProductList();
+        model.addAttribute("products", products);
+
+        return "productList";
+    }
+
+}
